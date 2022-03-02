754c68655328d67efda79cfff4780768e465981d
Nada Amin <namin@seas.harvard.edu>
2022-03-02 05:00:13 -0500

    convert from Java to Python

--- stringprops.py
+++ stringprops.py
@@ -1,54 +1,62 @@
-# TODO: define an API and convert Java code to Python
+# TODO: define an API
+
+import re
+
+str = "dummy"
+integer = 1
+outputStr = "dummy"
 
 # Listing 5: Java code for all Properties acting on single Strings.
-str.isEmpty()               # is empty?
-str.length() == 1           # is single char?
-str.length() <= 5           # is short string?
-str.equals(lower)           # is lowercase?
-str.equals(upper)           # is uppercase?
-str.contains(" ")           # contains space?
-str.contains(",")           # contains comma?
-str.contains(".")           # contains period?
-str.contains("-")           # contains dash?
-str.contains("/")           # contains slash?
-str.matches(". *\\d. *")    # contains digits?
-str.matches("\\d+")         # only digits?
-str.matches(". *[a-zA-Z]. * ") # contains letters?
-str.matches("[a-zA-Z]+")    # only letters?
+str == ""                   # is empty?
+len(str) == 1               # is single char?
+len(str) <= 5               # is short string?
+str.islower()               # is lowercase?
+str.isupper()               # is uppercase?
+" " in str                  # contains space?
+"," in str                  # contains comma?
+"." in str                  # contains period?
+"-" in str                  # contains dash?
+"/" in str                  # contains slash?
+bool(re.search(r"\d", str))        # contains digits?
+bool(re.match(r"\d+$", str))       # only digits?
+bool(re.search(r"\d", str))        # contains digits?
+bool(re.match(r"\d+$", str))       # only digits?
+bool(re.search(r"[a-zA-Z]", str))  # contains letters?
+bool(re.match(r"[a-zA-Z]+$", str)) # only letters?
 
 # Listing 6: Java code for all Properties acting on single Integers.
 integer == 0                # is zero?
 integer == 1                # is one?
 integer == 2                # is two?
 integer < 0                 # is negative?
-0 < integer && integer <= 3 # is small integer?
-3 < integer && integer <= 9 # is medium integer?
+0 < integer and integer <= 3  # is small integer?
+3 < integer and integer <= 9  # is medium integer?
 9 < integer                 # is large integer?
 
 # Listing 7: Java code for all Properties acting on a String and the output String.
-outputStr.contains(str)            # output contains input?
-outputStr.startsWith(str)          # output starts with input?
-outputStr.endsWith(str)            # output ends with input?
-str.contains(outputStr)            # input contains output?
-str.startsWith(outputStr)          # input starts with output?
-str.endsWith(outputStr)            # input ends with output?
-outputStrLower.contains(lower)     # output contains input ignoring case?
-outputStrLower.startsWith(lower)   # output starts with input ignoring case?
-outputStrLower.endsWith(lower)     # output ends with input ignoring case?
-lower.contains(outputStrLower)     # input contains output ignoring case?
-lower.startsWith(outputStrLower)   # input starts with output ignoring case?
-lower.endsWith(outputStrLower)     # input ends with output ignoring case?
-str.equals(outputStr)              # input equals output?
-lower.equals(outputStrLower)       # input equals output ignoring case?
-str.length() == outputStr.length() # input same length as output?
-str.length() < outputStr.length()  # input shorter than output?
-str.length() > outputStr.length()  # input longer than output?
+str in outputStr                   # output contains input?
+outputStr.startswith(str)          # output starts with input?
+outputStr.endswith(str)            # output ends with input?
+outputStr in str                   # input contains output?
+str.startswith(outputStr)          # input starts with output?
+str.endswith(outputStr)            # input ends with output?
+str.lower() in outputStr.lower()   # output contains input ignoring case?
+outputStr.lower().startswith(str.lower()) # output starts with input ignoring case?
+outputStr.lower().endswith(str.lower())   # output ends with input ignoring case?
+outputStr.lower() in str.lower()          # input contains output ignoring case?
+str.lower().startswith(outputStr.lower())  # input starts with output ignoring case?
+str.lower().endswith(outputStr.lower())    # input ends with output ignoring case?
+str == outputStr                    # input equals output?
+str.lower() == outputStr.lower()    # input equals output ignoring case?
+len(str) == len(outputStr)          # input same length as output?
+len(str) < len(outputStr)           # input shorter than output?
+len(str) > len(outputStr)           # input longer than output?
 
 # Listing 8: Java code for all Properties acting on an Integer and the output String.
-integer < outputStr.length()                # is less than output length?
-integer <= outputStr.length()               # is less or equal to output length?
-integer == outputStr.length()               # is equal to output length?
-integer >= outputStr.length()               # is greater or equal to output length?
-integer > outputStr.length()                # is greater than output length?
-Math.abs(integer - outputStr.length()) <= 1 # is very close to output length?
-Math.abs(integer - outputStr.length()) <= 3 # is close to output length?
+integer < len(outputStr)           # is less than output length?
+integer <= len(outputStr)          # is less or equal to output length?
+integer == len(outputStr)          # is equal to output length?
+integer >= len(outputStr)          # is greater or equal to output length?
+integer > len(outputStr)           # is greater than output length?
+abs(integer - len(outputStr)) <= 1 # is very close to output length?
+abs(integer - len(outputStr)) <= 3 # is close to output length?
