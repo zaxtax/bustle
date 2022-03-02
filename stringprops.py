99c92b96089add6e6672677ac52f76c04eaa1988
Nada Amin <namin@seas.harvard.edu>
2022-03-02 05:21:30 -0500

    include types

--- stringprops.py
+++ stringprops.py
@@ -3,7 +3,7 @@
 import re
 
 # Listing 5: Java code for all Properties acting on single Strings.
-props_str = [
+props_str = (('str',), [
 lambda str: str == ""                  , # is empty?
 lambda str: len(str) == 1              , # is single char?
 lambda str: len(str) <= 5              , # is short string?
@@ -20,10 +20,10 @@ lambda str: bool(re.search(r"\d", str))       , # contains digits?
 lambda str: bool(re.match(r"\d+$", str))      , # only digits?
 lambda str: bool(re.search(r"[a-zA-Z]", str)) , # contains letters?
 lambda str: bool(re.match(r"[a-zA-Z]+$", str)), # only letters?
-]
+])
 
 # Listing 6: Java code for all Properties acting on single Integers.
-props_int = [
+props_int = (('int',), [
 lambda integer: integer == 0               , # is zero?
 lambda integer: integer == 1               , # is one?
 lambda integer: integer == 2               , # is two?
@@ -31,10 +31,10 @@ lambda integer: integer < 0                , # is negative?
 lambda integer: 0 < integer and integer <= 3 , # is small integer?
 lambda integer: 3 < integer and integer <= 9 , # is medium integer?
 lambda integer: 9 < integer                , # is large integer?
-]
+])
 
 # Listing 7: Java code for all Properties acting on a String and the output String.
-props_str2str = [
+props_str2str = (('str','str'), [
 lambda str, outputStr: str in outputStr                  , # output contains input?
 lambda str, outputStr: outputStr.startswith(str)         , # output starts with input?
 lambda str, outputStr: outputStr.endswith(str)           , # output ends with input?
@@ -52,10 +52,10 @@ lambda str, outputStr: str.lower() == outputStr.lower()   , # input equals outpu
 lambda str, outputStr: len(str) == len(outputStr)         , # input same length as output?
 lambda str, outputStr: len(str) < len(outputStr)          , # input shorter than output?
 lambda str, outputStr: len(str) > len(outputStr)          , # input longer than output?
-]
+])
 
 # Listing 8: Java code for all Properties acting on an Integer and the output String.
-props_int2str = [
+props_int2str = (('int','str'), [
 lambda integer, outputStr: integer < len(outputStr)          , # is less than output length?
 lambda integer, outputStr: integer <= len(outputStr)         , # is less or equal to output length?
 lambda integer, outputStr: integer == len(outputStr)         , # is equal to output length?
@@ -63,4 +63,4 @@ lambda integer, outputStr: integer >= len(outputStr)         , # is greater or e
 lambda integer, outputStr: integer > len(outputStr)          , # is greater than output length?
 lambda integer, outputStr: abs(integer - len(outputStr)) <= 1, # is very close to output length?
 lambda integer, outputStr: abs(integer - len(outputStr)) <= 3, # is close to output length?
-]
+])
