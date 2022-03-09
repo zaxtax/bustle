3fecc6a9c2e852e735a5d9fe6e11f0fbb526d041
Nada Amin <namin@seas.harvard.edu>
2022-03-09 14:54:23 -0500

    DSL parser

---
+++ dslparser.py
@@ -0,0 +1,49 @@
+import tokenize
+import io
+
+def tokens(inp):
+    ts = tokenize.generate_tokens(io.StringIO(inp).readline)
+    return [x[1] for x in ts if x[1]!='']
+    
+def parset(dsl, ts):
+    if ts[0] in dsl.Ops:
+        op = ts[0]
+        ts = ts[1:]
+        assert ts[0] == '('
+        ts = ts[1:]
+        (returntype, argtypes) = dsl.types(op)
+        n = len(argtypes)
+        args = []
+        for i in range(n):
+            (arg, ts) = parset(dsl, ts)
+            args.append(arg)
+            if i!=n-1:
+                assert ts[0] == ','
+                ts = ts[1:]
+        assert ts[0] == ')'
+        ts = ts[1:]
+        return ((op, args), ts)
+    elif ts[0].startswith("x"):
+        x = ts[0]
+        try:
+            r = int(x[1:])
+        except ValueError:
+            assert False, "expected an input (x followed by number), not %s" % x
+        return (("input", r), ts[1:])
+    else:
+        try:
+            r = int(ts[0])
+            return (r, ts[1:])
+        except ValueError:
+            assert False, "unexpected token "+ts[0]
+
+def parse(dsl, inp):
+    (exp, ts) = parset(dsl, tokens(inp))
+    assert ts == []
+    return exp
+
+def test():
+    from bustle import ArithDsl
+    al = ArithDsl()
+    assert ("add", [("input", 0), 1]) == parse(al, "add(x0, 1)")
+    assert ("if", [("lt", [("input", 0), ("input", 1)]), 1, 0]) == parse(al, "if(lt(x0, x1), 1, 0)")
